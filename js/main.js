1532f30213e78a42727865ad5d6b940622888ef4
belmoussaoui <emb-12@outlook.com>
2021-08-04 09:26:46 +0200

    Intégration des assets de Cécile

--- js/main.js
+++ js/main.js
@@ -24,9 +24,20 @@ class SceneBoot extends Phaser.Scene {
         this.load.image('action1', './assets/img/left.png');
         this.load.image('action2', './assets/img/forward.png');
         this.load.image('action3', './assets/img/right.png');
-        this.load.image('player', './assets/img/player.png');
+        this.load.image('player', './assets/img/robot.png');
+        this.load.image('direction', './assets/img/player.png');
         this.load.image('scheduler', './assets/img/scheduler.png');
         this.load.image('stars', './assets/img/stars.png');
+        this.load.image('title', './assets/img/title.png');
+        this.load.image('hello', './assets/img/hello.png');
+        this.load.image('star', './assets/img/star.png');
+        this.load.image('child', './assets/img/child.png');
+        this.load.image('map', './assets/img/map.png');
+        this.load.image('actions', './assets/img/actions.png');
+        this.load.image('commands', './assets/img/commands.png');
+        this.load.image('dog', './assets/img/dog.png');
+        this.load.image('cat', './assets/img/cat.png');
+        this.load.image('ending', './assets/img/ending.png');
         this.load.json('maps', './data/maps.json');
     }
 }
@@ -49,15 +60,17 @@ class SceneTitle extends Phaser.Scene {
     createLogo() {
         let w = this.sys.game.canvas.width / 2;
         let h = this.sys.game.canvas.height / 2;
-        this.logo = this.add.image(w, h - 20, 'logo1');
+        this.child = this.add.image(w, h + 120, 'child');
+        this.logo = this.add.image(w, h - 100, 'logo1');
         this.logo.alpha = 0;
+        this.child.alpha = 0;
         this.logo.scale = 0.33;
         this.tweens.add({
             alpha: 1,
-            targets: [this.logo],
+            targets: [this.logo, this.child],
             ease: 'Cubic.easeIn', 
             duration: 1000,
-            hold: 1000,
+            hold: 1500,
             repeat:0,
             yoyo: true,
             onComplete: () => {
@@ -65,27 +78,45 @@ class SceneTitle extends Phaser.Scene {
                 this.createTitle();
             }
         });
+      
     }
 
     createTitle() {
         let w = this.sys.game.canvas.width / 2;
         let h = this.sys.game.canvas.height / 2;
-        this.title = this.add.text(w, 400, 'Learn Coding', {
+        this.title = this.add.image(w, 450, 'title');
+        this.title.scale = 0.5;
+        this.add.text(w, 520, 'CODE', {
             fontFamily: 'Helvetica',
             fontStyle: 'bold',
             color: '#fff',
             fontSize: '64px',
         }).setOrigin(0.5, 0.5);
+        this.hello = this.add.image(w, h * 2, 'hello').setOrigin(0.5, 0);
+        this.tweens.add({
+            y: 600,
+            targets: [this.hello],
+            ease: 'Cubic.easeIn', 
+            duration: 1000,
+            hold: 1000,
+            repeat:0,
+        });
+        this.hello.scale = 0.75;
   
         this.tweens.add({
-            y: 390,
+            y: 440,
             targets: this.title,
             duration: 1500,
             repeat:-1,
             yoyo: true
         });
 
-        this.add.text(w, h * 2 - 50, 'orlean games © 2021 july', {
+        this.add.text(w, h * 2 - 50, 'orlean games © 2021 august', {
+            fontFamily: 'Helvetica',
+            fontStyle: 'bold',
+            fontSize: '20px',
+        }).setOrigin(0.5, 0.5);
+        this.add.text(w, h * 2 - 50 + 25, 'written by bilal el moussaoui', {
             fontFamily: 'Helvetica',
             fontStyle: 'bold',
             fontSize: '20px',
@@ -97,8 +128,8 @@ class SceneTitle extends Phaser.Scene {
 
     createPlay() {
         let w = this.sys.game.canvas.width / 2;
-        this.play = this.add.image(w, 700, 'play1').setOrigin(.5, .5);
-        this.play.scale = 1.3;
+        this.play = this.add.image(w, 280, 'play1').setOrigin(.5, .5);
+        this.play.scale = 1.5;
         this.play.setInteractive();
         this.play.on('pointerover', () => this.play.setTexture('play2'));
         this.play.on('pointerout', () => this.play.setTexture('play1'));
@@ -145,53 +176,117 @@ class SceneGame extends Phaser.Scene {
         const w = 96;
         const h = 96;
         const mt = gameMap.mt;
+        const ml = 96 / 2
+        let map = this.add.image(286, 530, 'map');
         for (let i = 0; i < gameMap.data.length; i++) {
             const data = gameMap.data[i];
             if (data === 1) {
                 continue;
             }
-            const x = (i % 6) * w + w / 2;
-            const y = Math.floor(i / 6) * h + mt;
+            const x = (i % 5) * w + w / 2 + ml;
+            const y = Math.floor(i / 5) * h + mt;
             let c = 0xffffff;
-            if (i === gameMap.start) {
-                c = 0x50c878;
-            }
-            if (i === gameMap.end) {
-                c = 0xd2042d;
-            }
             const tile = this.add.rectangle(x, y, w, h, c);
-            tile.setStrokeStyle(2, 0x000000);
+            if (i === gameMap.start) {
+                const tile = this.add.rectangle(x, y, w-4, h-4, c);
+                this.tile = tile;
+                this.tile.setFillStyle(0x50c878);
+                this.tile.alpha = 0.8;
+                this.tween = this.tweens.add({
+                    targets: this.tile,
+                    alpha: 0.3, 
+                    duration: 1500,
+                    repeat: -1,
+                    yoyo: true,
+                });
+                this.tile.setDepth(2);
+            }
+            tile.setStrokeStyle(4, 0x000000);
             tile.id = i;
             tile.setInteractive().on("pointerdown", event => {
             },
             this);
+            if (i === gameMap.end) {
+                // c = 0xd2042d;
+                this.star = this.add.image(x, y, 'star');
+                this.star.scale = 0.25;
+                // this.star.setDepth(99);
+                
+            }
             this.tiles.push(tile);
         }
+        map.scale= 0.875;
     }
-
+    
     createActions() {
+        let w = this.sys.game.canvas.width / 2;
+        let board = this.add.image(w, 900, 'actions');
+        board.scale = 0.8;
+        this.canAction = true;
+        let colors = ['0xcfdb32', '0xf36e38', '0x87789c']
         for (let i = 0; i < gameMap.actions.length; i++) {
+            if (gameMap.id === 1 && i !== 1) {
+                continue;
+            }
             const action = gameMap.actions[i];
-            const tile = this.add.rectangle(208 + i * 80, 900, 80, 80, '0xffffff').setStrokeStyle(2, 0x000000);
+            const tile = this.add.circle(208 + i * 80 - 30 + i * 25, 900, 40, colors[i]);
             tile.action = action;
+            const image = this.add.image(208 + i * 80 - 30 + i * 25, 900, `action${action}`);
             tile.setInteractive().on("pointerdown", event => {
+                if (!this.canAction) {
+                    return;
+                }
+                this.canAction = false;
+                this.tween.stop();
+                this.tweens.add({
+                    y: tile.y + 6,
+                    targets: [tile, image],
+                    ease: 'Cubic.easeIn', 
+                    duration: 0,
+                    hold: 200,
+                    repeat:0,
+                    onComplete: () => {
+                        tile.y -= 6;
+                        image.y -= 6;
+                        this.canAction = true;
+                    }
+                });
+                this.image.alpha = 1;
                 this.addCommand(tile.action);
             }, this);
-            const image = this.add.image(208 + i * 80, 900, `action${action}`);
             image.scale = 0.8;
+            if (gameMap.id === 1) {
+                if (i === 1) {
+                    this.image = image;
+                    this.tween = this.tweens.add({
+                        targets: image,
+                        alpha: 0.4,
+                        duration: 1000,
+                        repeat: -1,
+                        yoyo: true,
+                    });
+                } else {
+                    image.alpha = 0.1;
+                }
+            }
         }
     }
 
     createCommands() {
+        let w = this.sys.game.canvas.width / 2;
+        let c = this.add.image(w, 150, 'commands');
+        c.angle = 1;
+
         for (let i = 0; i < gameMap.commands; i++) {
             const action = gameMap.actions[i];
             let w = 80;
             let x = this.sys.game.canvas.width / 2 - (gameMap.commands * w / 2) + w / 2;
-            const tile = this.add.rectangle(x + i * w, 150, w, w, '0xffffff').setStrokeStyle(2, 0x000000);
+            const tile = this.add.rectangle(x + i * w, 150, w, w, '0xffffff').setStrokeStyle(4, 0x000000);
            
             tile.action = action;
             tile.setInteractive().on("pointerdown", event => {
             }, this);
+            
             // const image = this.add.image(200 + i * 80, 100, `action${action}`);
             // image.scale = 0.8;    
         }
@@ -230,6 +325,7 @@ class SceneGame extends Phaser.Scene {
 
     update() {
         this.player.update();
+        this.star.angle += 0.5;
     }
 
   
@@ -239,13 +335,15 @@ class GameMap {
         this.id = 1;
         this.data = [];
         this.actions = [];
+        this.direction = 0;
         this.start = 0;
         this.end = 0;
-        this.mt = (1024 - 6 * 96) / 2 + 96 / 2;
+        this.mt = (1024 - 6 * 96) / 2 + 96;
+        this.ml = 96 / 2;
     }
 
     row() {
-        return 6;
+        return 5;
     }
 
     setup(scene) {
@@ -253,6 +351,7 @@ class GameMap {
         this.commands = data.commands;
         this.actions = [1, 2, 3];
         this.data = data.map
+        this.direction = data.direction;
         this.start = data.start;
         this.end = data.end;
     }
@@ -282,7 +381,7 @@ class GameMap {
     }
 
     startX() {
-        return (this.start % this.row()) * this.tileWidth() + 96;
+        return (this.start % this.row()) * this.tileWidth() * 2 + this.ml + 48;
     }
     
     startY() {
@@ -296,20 +395,41 @@ class Player extends Phaser.GameObjects.Sprite {
         super(scene, x, y, 'player');
         this.startX = x;
         this.startY = y;
-        this.direction = 0;
+        this.direction = gameMap.direction;
+        // this.angle = -this.direction * 90
         this.routes = [];
         this.state = 'move';
+        this.shiftY = 0
         this.index = -1;
+        this.scale = 0.22;
+        this.arrow = this.scene.add.image(this.x, this.y, 'direction');
+        this.arrow.setDepth(100);
+        this.arrow.angle = -this.direction * 90;
+        this.arrow.scale = 0.60;
+        this.setOrigin(0.5, 0.6);
+        this.anim = scene.tweens.add({
+            y: this.y - 5,
+            targets: this,
+            duration: 1200,
+            repeat:-1,
+            yoyo: true
+        });
+        this.setDepth(3);
     }
 
     canPass() {
-        let new_x = (this.x + this.xWithDirection(this.direction)) / 96 - 0.5;
-        let new_y = (this.y + this.yWithDirection(this.direction) - gameMap.mt) / 96;
-            console.log(new_x, new_y);
+        let new_x = Math.round((this.x + this.xWithDirection(this.direction) - gameMap.ml) / 96 - 0.5);
+        console.log(new_x);
+        let new_y = Math.round((this.y + this.yWithDirection(this.direction) - gameMap.mt) / 96);
+        if (new_x >= gameMap.row() || new_y >= gameMap.row() || new_x < 0 || new_y < 0) {
+            return false;
+        }
         return gameMap.data[new_y * gameMap.row() + new_x] === 0;
     }
 
     update() {
+        this.arrow.x = this.x;
+        this.arrow.y = this.y + 15;
         if (this.state === 'move' && this.scene.play) {
             if (this.checkVictory()) {
                 gameMap.id++;
@@ -328,6 +448,8 @@ class Player extends Phaser.GameObjects.Sprite {
                 this.index++;
                 this.scene.scheduler.x = x + this.index * w;
                 this.processMoveCommand(route);
+                this.scene.tile.y = Math.round((this.y - gameMap.mt) / 96) * 96 + gameMap.mt;
+                this.scene.tile.x = this.x;
             } else {
                 this.clear();
             }
@@ -336,22 +458,33 @@ class Player extends Phaser.GameObjects.Sprite {
     }
 
     checkVictory() {
-        let new_x = this.x / 96 - 0.5;
-        let new_y = (this.y - gameMap.mt) / 96;
+        let new_x = (this.x - gameMap.ml) / 96 - 0.5;
+        let new_y = Math.round((this.y - gameMap.mt) / 96);
         return new_y * gameMap.row() + new_x === gameMap.end;
     }
 
     clear() {
+        this.anim.stop();
         this.x = this.startX;
         this.y = this.startY;
-        this.angle = 0;
-        this.direction = 0;
+        this.arrow.angle = -gameMap.direction * 90;
+        this.direction = gameMap.direction;
         this.scene.clear();
         this.index = -1;
         let w = 80;
         let x = this.scene.sys.game.canvas.width / 2 - (gameMap.commands * w / 2) + w / 2;
         this.scene.scheduler.x = x + this.index * w;
         this.scene.scheduler.visible = false;
+        this.scene.tile.y = this.y;
+        this.scene.tile.x = this.x;
+        this.anim = this.scene.tweens.add({
+            y: this.y - 5,
+            targets: this,
+            duration: 1200,
+            repeat:-1,
+            yoyo: true
+        });
+        
     }
 
     processMoveCommand(command) {
@@ -368,10 +501,19 @@ class Player extends Phaser.GameObjects.Sprite {
     }
 
     moveForward() {
+        this.anim.complete();
         if (this.canPass()) {
             this.x += this.xWithDirection();
             this.y += this.yWithDirection();
+            this.anim = this.scene.tweens.add({
+                y: this.y - 5,
+                targets: this,
+                duration: 1200,
+                repeat:-1,
+                yoyo: true
+            });
         } else {
+            this.anim.stop();
             this.scene.tweens.add({
                 targets: this,
                 y: this.y - 10,
@@ -411,7 +553,7 @@ class Player extends Phaser.GameObjects.Sprite {
 
     turnLeft() {
         this.direction = (this.direction + 1) % 4;
-        this.angle -= 90;
+        this.arrow.angle -= 90;
     }
 
     turnRight() {
@@ -419,8 +561,9 @@ class Player extends Phaser.GameObjects.Sprite {
         if (this.direction === -1) {
             this.direction = 3;
         }
-        this.angle += 90;
+        this.arrow.angle += 90;
     }
+
 }
 
 class SceneOver extends Phaser.Scene {
@@ -428,11 +571,30 @@ class SceneOver extends Phaser.Scene {
         super();
     }
 
+    init() {
+        gameMap.id = 1;
+    }
+
     create() {
         let w = this.sys.game.canvas.width / 2;
         let h = this.sys.game.canvas.height / 2;
         let stars = this.add.image(w, h, 'stars');
-        this.add.text(w, h, "Congratulation!", {
+        let dog = this.add.image(w + 220, h + 280, 'dog');
+        let cat = this.add.image(w - 240, h + 290, 'cat');
+        let ending = this.add.image(w, -400, 'ending');
+        ending.angle = 180;
+        ending.scale = 0.7;
+        this.tweens.add({
+            y: -50,
+            targets: ending,
+            ease: 'Cubic.easeIn', 
+            duration: 1000,
+            hold: 1000,
+            repeat:0,
+        });
+        dog.scale = 0.75
+        cat.scale = 0.7
+        this.add.text(w, h, "Bravo!", {
             fontFamily: 'Helvetica',
             fontStyle: 'bold',
             color: '#fff',
@@ -446,7 +608,7 @@ class SceneOver extends Phaser.Scene {
             repeat:-1,
             yoyo: true,
         });
-        this.play = this.add.image(w, 700, 'back1').setOrigin(.5, .5);
+        this.play = this.add.image(w, 360, 'back1').setOrigin(.5, .5);
         this.play.scale = 2;
         this.play.setInteractive();
         this.play.on('pointerover', () => this.play.setTexture('back2'));
